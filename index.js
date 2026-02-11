7133c55283714aea93d5e4036966d616c9b5d00d
Matheus805-beep <matheussantosporto1@gmail.com>
2026-02-11 10:22:08 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-const { 
+const { REST, Routes, SlashCommandBuilder } = require('discord.js');
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName('painel')
+    .setDescription('Enviar painel de fila')
+    .toJSON()
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log('Registrando comandos...');
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      { body: commands }
+    );
+    console.log('Comandos registrados com sucesso!');
+  } catch (error) {
+    console.error(error);
+  }
+})();const { 
   Client, 
   GatewayIntentBits, 
   EmbedBuilder, 
